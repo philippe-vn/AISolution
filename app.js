d98915cddb59f379ce49838e303501ef4ff575f3
philippe-vn <van.nieuwenhuyze@telenet.be>
2020-05-07 07:22:57 +0200

    server test

--- app.js
+++ app.js
@@ -19,19 +19,16 @@
 // });
 
 // EXAMPLE
-const http = require('http');
+const http = require('http')
 
-const hostname = 'www.ai-solution.be';
-const port = 80;
+const port = process.env.PORT
 
 const server = http.createServer((req, res) => {
-   res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-  
-  
- });
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end('<h1>Hello, World!</h1>')
+})
 
- server.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`);
- });
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
