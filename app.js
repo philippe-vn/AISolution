34de7bcd7822aa014c79af62bcf963d2c4f673e1
philippe-vn <van.nieuwenhuyze@telenet.be>
2020-05-07 07:08:13 +0200

    Test appCode

--- app.js
+++ app.js
@@ -1,66 +1,36 @@
-const express = require("express");
-const port = 3000;
-const app = express();
+// const express = require("express");
+// const port = 80;
+// const app = express();
 
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+// app.listen(port);
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        console.log("Error: current day is equal to: " + currentDay);
+// res.sendFile(__dirname + "/index.html");
 
-
-    }
-
-res.render('list', {kindOfDay: day});
-
-
-
-
-    
-    // res.sendFile(__dirname + "/index.html") == STATIC USE WEBSITE
-});
-
-
-
-
-
-
-
-
-
-
-
-
+// == STATIC USE WEBSITE
 
 //Console
-app.listen(port, function(){
-    console.log("Server is running on port 3000");
-});
+// app.listen(port, function(){
+    // console.log("Server is running on port 3000");
+// });
+
+// EXAMPLE
+const http = require('http');
+
+ const hostname = '127.0.0.1';
+ const port = 80;
+
+const server = http.createServer((req, res) => {
+   res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+  
+ });
+
+ server.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+ });
