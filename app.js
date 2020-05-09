05f1afeeb9ca17f5424e17a36769911852eaf0d5
philippe-vn <van.nieuwenhuyze@telenet.be>
2020-05-09 16:43:43 +0200

    change css

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
-// const express = require("express");
-// const port = 80;
-// const app = express();
+const express = require("express");
+const port = 5000;
+const app = express();
 
 
-// app.set('view engine', 'ejs');
 
-// app.use(express.static("public"));
 
-// app.listen(port);
+app.use(express.static("public"));
 
-// res.sendFile(__dirname + "/index.html");
+
+app.set('view engine', 'ejs');
+
+res.sendFile(__dirname + "/index.html");
 
 // == STATIC USE WEBSITE
 
 //Console
-// app.listen(port, function(){
-    // console.log("Server is running on port 3000");
-// });
+app.listen(port, function(){
+    console.log("Server is running on port 3000");
+});
 
-// EXAMPLE
-const http = require('http')
-
-const port = process.env.PORT
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello, World!</h1>')
-})
-
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`)
-})
